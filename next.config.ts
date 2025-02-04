01de44fa2cf30cb641c9302356a63e22997e9caa
ahmaddeveloper12 <codewithahmad363@gmail.com>
2025-02-04 01:37:26 -0800

    kk

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
+module.exports = {
+  images: {
+    domains: [
+      'image.freepik.com', // Add Freepik domain here
+      'media.istockphoto.com', // Example domain if you're using iStock images
+      // Add other domains that you plan to use
+    ],
+  },
 };
-
-export default nextConfig;
